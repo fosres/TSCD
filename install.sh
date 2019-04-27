7b979758d2698adaf8afdd6b398024f90c869de9
Tanveer Salim <tanveerasalim@gmail.com>
2019-04-26 23:45:02 -0500

    master

--- install.sh
+++ install.sh
@@ -1,3 +1,36 @@
+: '
+Copyright (C) Tanveer Salim 2018-INFINITY
+
+NOTICE: All software in this repository
+
+comes with absolutely
+
+NO WARRANTY and provided "as is" basis.
+
+This software and all other software in
+
+this repository is distributed with a GNU GPL
+
+License v2.0. All are free to copy, share, 
+	
+	
+distribute, and/or modify this software,
+	
+even commercially as long as it compliant
+
+with GNU GPL v2.0 LICENSING terms as well
+
+as the terms of this copyright and license
+
+statement.
+
+All software that was inspired
+
+or is a derivative of this work must have
+
+the exact same LICENSE and copyright permissions.
+'
+
 #!/bin/bash
 
 gcc src/tscd6.c -o tscd
